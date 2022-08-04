653dc747b5fe8d0f5b75931fe60dd72e08b06f7e
Aysenur Alper <72700879+aysenuralper@users.noreply.github.com>
2022-08-04 12:28:25 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace aysenurkoycu
+{
+    partial class FormMainMenu
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu));
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnCustomers = new System.Windows.Forms.Button();
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.btnproducts = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelTitleBar = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelDesktopPane = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            this.panelTitleBar.SuspendLayout();
+            this.panelDesktopPane.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelMenu.Controls.Add(this.btnCustomers);
+            this.panelMenu.Controls.Add(this.btnOrders);
+            this.panelMenu.Controls.Add(this.btnproducts);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(220, 550);
+            this.panelMenu.TabIndex = 0;
+            // 
+            // btnCustomers
+            // 
+            this.btnCustomers.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnCustomers.FlatAppearance.BorderSize = 0;
+            this.btnCustomers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCustomers.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnCustomers.Image = ((System.Drawing.Image)(resources.GetObject("btnCustomers.Image")));
+            this.btnCustomers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomers.Location = new System.Drawing.Point(0, 200);
+            this.btnCustomers.Name = "btnCustomers";
+            this.btnCustomers.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnCustomers.Size = new System.Drawing.Size(220, 60);
+            this.btnCustomers.TabIndex = 3;
+            this.btnCustomers.Text = "ANA SAYFA";
+            this.btnCustomers.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCustomers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCustomers.UseVisualStyleBackColor = true;
+            this.btnCustomers.Click += new System.EventHandler(this.btnCustomers_Click);
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOrders.FlatAppearance.BorderSize = 0;
+            this.btnOrders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrders.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnOrders.Image = ((System.Drawing.Image)(resources.GetObject("btnOrders.Image")));
+            this.btnOrders.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrders.Location = new System.Drawing.Point(0, 140);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnOrders.Size = new System.Drawing.Size(220, 60);
+            this.btnOrders.TabIndex = 2;
+            this.btnOrders.Text = " GÖNDERİLENLER";
+            this.btnOrders.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrders.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOrders.UseVisualStyleBackColor = true;
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // btnproducts
+            // 
+            this.btnproducts.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnproducts.FlatAppearance.BorderSize = 0;
+            this.btnproducts.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnproducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnproducts.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnproducts.Image = ((System.Drawing.Image)(resources.GetObject("btnproducts.Image")));
+            this.btnproducts.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnproducts.Location = new System.Drawing.Point(0, 80);
+            this.btnproducts.Name = "btnproducts";
+            this.btnproducts.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnproducts.Size = new System.Drawing.Size(220, 60);
+            this.btnproducts.TabIndex = 1;
+            this.btnproducts.Text = " YENİ İLETİ";
+            this.btnproducts.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnproducts.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnproducts.UseVisualStyleBackColor = true;
+            this.btnproducts.Click += new System.EventHandler(this.btnproducts_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(50)))));
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(220, 80);
+            this.panelLogo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(42, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AYSENUR KOYCU";
+            // 
+            // panelTitleBar
+            // 
+            this.panelTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.panelTitleBar.Controls.Add(this.lblTitle);
+            this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitleBar.Location = new System.Drawing.Point(220, 0);
+            this.panelTitleBar.Name = "panelTitleBar";
+            this.panelTitleBar.Size = new System.Drawing.Size(628, 80);
+            this.panelTitleBar.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Mongolian Baiti", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(289, 24);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(79, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HOME";
+            // 
+            // panelDesktopPane
+            // 
+            this.panelDesktopPane.Controls.Add(this.button4);
+            this.panelDesktopPane.Controls.Add(this.textBox1);
+            this.panelDesktopPane.Controls.Add(this.button3);
+            this.panelDesktopPane.Controls.Add(this.button2);
+            this.panelDesktopPane.Controls.Add(this.button1);
+            this.panelDesktopPane.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktopPane.Location = new System.Drawing.Point(220, 80);
+            this.panelDesktopPane.Name = "panelDesktopPane";
+            this.panelDesktopPane.Size = new System.Drawing.Size(628, 470);
+            this.panelDesktopPane.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(417, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "MAİL GRUBUNA ADRES EKLE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 42);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(458, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 45);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "BAĞLANTI HATASI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(328, 404);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 45);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "HIRSIZ ALARMI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "YANGIN ALARMI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(848, 550);
+            this.Controls.Add(this.panelDesktopPane);
+            this.Controls.Add(this.panelTitleBar);
+            this.Controls.Add(this.panelMenu);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "FormMainMenu";
+            this.Text = "FormMainMenu";
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            this.panelLogo.PerformLayout();
+            this.panelTitleBar.ResumeLayout(false);
+            this.panelTitleBar.PerformLayout();
+            this.panelDesktopPane.ResumeLayout(false);
+            this.panelDesktopPane.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Button btnCustomers;
+        private System.Windows.Forms.Button btnOrders;
+        private System.Windows.Forms.Button btnproducts;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Panel panelTitleBar;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panelDesktopPane;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.IO;
+
+
+namespace aysenurkoycu
+{
+    public partial class FormMainMenu : Form
+    {
+        
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        private Form activeForm;
+        
+
+        public void dosyaac()
+        {
+            StreamWriter sw = new StreamWriter("d:\\kullanicilistesi.txt");
+            sw.Close();
+        }
+        public void dosyayaz()
+        {
+            StreamWriter writer = File.AppendText("d:\\kullanicilistesi.txt");
+            writer.WriteLine("zx.mopafk@gmail.com\n"
+                 + "as.mopafk@gmail.com\n" +
+                 "qw.mopafk@gmail.com\n" +
+                 "rt.mopafk@gmail.com\n" +
+                 "ty.mopafk@gmail.com\n" +
+                 "yu.mopafk@gmail.com\n" +
+                 "uı.mopafk@gmail.com\n" +
+                 "bn.mopafk@gmail.com");
+            
+
+            
+
+
+
+            writer.Close();
+        }
+        public void dosyalistele()
+        {
+            StreamReader sr = File.OpenText("d:\\kullanicilistesi.txt");
+            List<string> satirlarList = new List<string>();
+            string satir = null;
+
+            while ((satir = sr.ReadLine()) != null)
+            {
+
+                satirlarList.Add(satir);
+                
+            }
+            foreach (string s in satirlarList)
+            {
+                kime = s;
+            }
+            sr.Close();
+        }
+        public static string kime, baslik, mesaj;
+
+        async public void mailgonderyangin()
+        {
+            StreamReader sr = File.OpenText("d:\\kullanicilistesi.txt");
+            List<string> satirlarList = new List<string>();
+            string satir = null;
+
+            while ((satir = sr.ReadLine()) != null)
+            {
+
+                satirlarList.Add(satir);
+                
+
+            }
+            foreach (string s in satirlarList)
+            {
+                kime = s;
+                baslik = "YANGIN ALARMI";
+                mesaj = "TESİSTE YANGIN ALARMI VERİLDİ. İTFAİYE VE AMBULANS EKİPLERİNE HABER VERİLDİ.";
+                await Task.Run(sender);
+
+
+            }
+            sr.Close();
+            
+        }
+        async private void mailgonderhirsiz()
+        {
+            StreamReader sr = File.OpenText("d:\\kullanicilistesi.txt");
+            List<string> satirlarList = new List<string>();
+            string satir = null;
+
+            while ((satir = sr.ReadLine()) != null)
+            {
+
+                satirlarList.Add(satir);
+                
+
+            }
+            foreach (string s in satirlarList)
+            {
+                kime = s;
+                baslik = "HIRSIZ ALARMI";
+                mesaj = "TESİSTE HIRSIZ ALARMI VERİLDİ. POLİS EKİPLERİNE HABER VERİLDİ.";
+                await Task.Run(sender);
+
+
+            }
+            sr.Close();
+            
+        }
+        public async Task sender()
+        {
+            MailMessage mesajım = new MailMessage();
+            SmtpClient istemci = new SmtpClient();
+            istemci.Credentials = new System.Net.NetworkCredential("aysenurkycdeneme@gmail.com", "123456C#");
+            istemci.Port = 587;
+            istemci.Host = "smtp.gmail.com";
+            mesajım.To.Add(kime);
+            mesajım.From = new MailAddress("aysenurkycdeneme@gmail.com");
+            mesajım.Subject = baslik;
+            mesajım.Body = mesaj;
+            istemci.EnableSsl = true;
+            istemci.Send(mesajım);
+        }
+        async public void mailgonderbaglantihatasi()
+        {
+            
+            StreamReader sr = File.OpenText("d:\\kullanicilistesi.txt");
+            List<string> satirlarList = new List<string>();
+            string satir = null;
+           
+
+            while ((satir = sr.ReadLine()) != null)
+            {
+                satirlarList.Add(satir);
+
+
+
+            }
+            foreach (string s in satirlarList)
+            {
+                kime = s;
+                baslik = "BAGLANTI HATASI";
+                mesaj = "TESİSTE BAĞLANTI HATASI ALARMI VERİLDİ. ARIZA EKİPLERİNE HABER VERİLDİ.";
+                await Task.Run(sender);
+
+
+            }
+            sr.Close();
+            
+        }
+
+        public FormMainMenu()
+        {
+            InitializeComponent();
+            random=new Random();
+            dosyaac();
+            dosyayaz();
+            
+            
+        }
+        
+        private Color SelectThemeColor()
+        {
+            int index = random.Next(ThemeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                index = random.Next(ThemeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = ThemeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SelectThemeColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                    panelTitleBar.BackColor = color;
+                    panelLogo.BackColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                    ThemeColor.PrimaryColor = color;
+                    ThemeColor.SecondaryColor = ThemeColor.ChangeColorBrightness(color, -0.3);
+                }
+
+            }
+
+        }
+        
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+                }
+            }
+        }
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            { 
+                activeForm.Close(); 
+            }
+                
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panelDesktopPane.Controls.Add(childForm);
+            this.panelDesktopPane.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitle.Text = childForm.Text;
+        }
+
+        private void btnproducts_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormProducts(), sender);
+        }
+
+        private void btnOrders_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormOrders(), sender);
+            
+                    }
+
+        private void btnCustomers_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormCustomers(), sender);
+            FormMainMenu menu = new FormMainMenu();
+            menu.Show();
+            this.Hide();
+
+            
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormReport(), sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mailgonderhirsiz();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mailgonderbaglantihatasi();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = File.AppendText("d:\\kullanicilistesi.txt");
+            
+            writer.WriteLine(textBox1.Text);
+            writer.Close();
+            textBox1.Clear();
+
+        }
+
+        private void btnNotifications_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormNatifications(), sender);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mailgonderyangin();
+        }
+    }
+}
+            
+            
+  
+        
